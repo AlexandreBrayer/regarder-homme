3d86c57ea04b2cbe7a51d83a9d51ef728f36363a
Alexandre Brayer <a.brayer@purjus.fr>
2024-02-02 17:46:50 +0100

    Update API base URL

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,11 @@
 import { API } from 'sveltekit-api';
+import { BASE_URL } from '$env/static/private';
 
 export default new API(import.meta.glob('./**/*.ts'), {
 	openapi: '3.0.0',
 	servers: [
 		{
-			url: process.env.BASE_URL || 'http://localhost:5173'
+			url: BASE_URL || 'http://localhost:5173'
 		}
 	],
 	info: {
