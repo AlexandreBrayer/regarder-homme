dd6b651608bc18222b7b398555489c6c8463b7e5
Alexandre Brayer <a.brayer@purjus.fr>
2024-02-02 17:39:09 +0100

    add server

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,6 +2,11 @@ import { API } from 'sveltekit-api';
 
 export default new API(import.meta.glob('./**/*.ts'), {
 	openapi: '3.0.0',
+	servers: [
+		{
+			url: process.env.BASE_URL || 'http://localhost:5173'
+		}
+	],
 	info: {
 		title: 'WatchMan API',
 		version: '1.0.0',
