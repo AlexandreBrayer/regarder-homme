7353d3ccdc3c11382b859d64a0191d4fbed50c34
Alexandre Brayer <a.brayer@purjus.fr>
2024-02-03 12:10:23 +0100

    [FIX] missing typing

--- src/api/spider/spawn/POST.ts
+++ src/api/spider/spawn/POST.ts
@@ -11,10 +11,9 @@ export const Input = z.object({
 	name: z.string()
 });
 
-export default async function () {
-	const name = 'test';
+export default async function (input: z.infer<typeof Input>): Promise<z.infer<typeof Output>>{
 	try {
-		spawnSpider(name);
+		spawnSpider(input.name);
 		return { status: 'success' };
 	} catch (error) {
 		console.error(`spawnSpider error: ${error}`);
