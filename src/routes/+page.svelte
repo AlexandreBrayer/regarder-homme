4f43bc4c64fbe30a75f9f0170ee1fba4e1c930f5
Alexandre Brayer <a.brayer@purjus.fr>
2024-02-24 20:12:53 +0100

    Update user greeting in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,14 @@
 	import QuickAccessCard from '$lib/components/ui/home/quickAccessCard.svelte';
 	import ScheduledScrapers from '$lib/components/ui/home/scheduledScrapers.svelte';
 	import AvailableScrapers from '$lib/components/ui/home/availableScrapers.svelte';
+	import { getContext } from 'svelte';
+	import type { UserContext } from '$lib/stores/session';
+	import type { Writable } from 'svelte/store';
+
+	const user = getContext<Writable<UserContext>>('user');
 </script>
 
-<h1 class="p-8 text-2xl font-bold">Bienvenue Alexandre</h1>
+<h1 class="p-8 text-2xl font-bold">Bienvenue {$user?.username}</h1>
 
 <div class="flex w-full items-start gap-8">
 	<QuickAccessCard class="w-1/3" />
